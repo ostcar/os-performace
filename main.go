ccdc3922ba634700646696566d832d418ab4230f
Oskar Hahn <mail@oshahn.de>
2021-02-19 15:05:30 +0100

    Keep open

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 // reading_client simulates clients, that login to openslides and after a
 // successfull login send all request, that the client usual sends.
 import (
+	"bufio"
+	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -13,6 +16,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/cheggaaa/pb/v3"
 )
@@ -32,15 +36,30 @@ func main() {
 }
 
 func run(args []string) error {
-	cfg, err := loadConfig(args)
+	cfg, err := loadConfig(args[1:])
 	if err != nil {
 		return fmt.Errorf("loading config: %w", err)
 	}
 
+	switch cfg.testCase {
+	case testBrowser:
+		err = runBrowser(cfg)
+	case testConnect:
+		err = runKeepOpen(cfg)
+	default:
+		err = fmt.Errorf("unknown testCase")
+	}
+
+	if err != nil {
+		return fmt.Errorf("running test: %w", err)
+	}
+	return nil
+}
+
+func runBrowser(cfg *Config) error {
 	log.Printf("using %d clients to %s", cfg.clientCount, cfg.domain)
 
 	bar := pb.StartNew(cfg.clientCount * 5)
-	defer bar.Finish()
 
 	clients := make([]*client, cfg.clientCount)
 	for i := range clients {
@@ -51,23 +70,90 @@ func run(args []string) error {
 		clients[i] = c
 	}
 
+	start := time.Now()
+
 	var wg sync.WaitGroup
 	for _, c := range clients {
 		wg.Add(1)
 		go func(c *client) {
 			defer wg.Done()
-			if err := c.run(); err != nil {
+			if err := c.browser(); err != nil {
 				log.Printf("Client failed: %v", err)
 			}
 		}(c)
 	}
 
 	wg.Wait()
+	bar.Finish()
+	log.Printf("Run for %v", time.Now().Sub(start))
 	return nil
 }
 
-type incrementer interface {
-	Increment() int
+func runKeepOpen(cfg *Config) error {
+	path := "/system/autoupdate"
+
+	c, err := newClient(cfg.domain, cfg.username, cfg.passwort, nil)
+	if err != nil {
+		return fmt.Errorf("creating client: %w", err)
+	}
+
+	if err := c.login(); err != nil {
+		return fmt.Errorf("login client: %w", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	for i := 0; i < cfg.clientCount; i++ {
+		wg.Add(1)
+		go func(first bool) {
+			defer wg.Done()
+
+			r, err := c.keepOpen(ctx, path)
+			if err != nil {
+				log.Println("Can not create connection: %w", err)
+				return
+			}
+			defer r.Close()
+
+			if first {
+				// TODO: Listen to ctx.Done
+				scanner := bufio.NewScanner(r)
+				scanner.Buffer(make([]byte, 10), 1_000_000)
+				for scanner.Scan() {
+					text := scanner.Text()
+					if len(text) > 50 {
+						text = text[:50] + fmt.Sprintf("... [%d bytes]", len(text))
+					}
+					log.Println(text)
+				}
+				if err := scanner.Err(); err != nil {
+					log.Println("Can not read body: %w", err)
+					return
+				}
+			} else {
+				readToNothing(ctx, r)
+			}
+
+		}(i == 0)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func readToNothing(ctx context.Context, r io.Reader) {
+	go func() {
+		var err error
+		buf := make([]byte, 1000)
+		for err == nil && ctx.Err() == nil {
+			_, err = r.Read(buf)
+		}
+	}()
+
+	<-ctx.Done()
+	return
 }
 
 type client struct {
@@ -99,13 +185,15 @@ func newClient(domain, username, password string, bar *pb.ProgressBar) (*client,
 	return c, nil
 }
 
-// run sends the normal requests
-func (c *client) run() error {
+// browser sends the request each browser tab sends.
+func (c *client) browser() error {
 	if err := c.login(); err != nil {
 		return fmt.Errorf("login client: %w", err)
 	}
 
-	c.bar.Increment()
+	if c.bar != nil {
+		c.bar.Increment()
+	}
 
 	var wg sync.WaitGroup
 
@@ -118,8 +206,12 @@ func (c *client) run() error {
 		wg.Add(1)
 		go func(path string) {
 			defer wg.Done()
-			defer c.bar.Increment()
-			if err := c.get(path); err != nil {
+
+			if c.bar != nil {
+				defer c.bar.Increment()
+			}
+
+			if err := c.get(context.Background(), path); err != nil {
 				log.Printf("Error get request to %s: %v", path, err)
 			}
 		}(path)
@@ -127,6 +219,19 @@ func (c *client) run() error {
 
 	wg.Wait()
 	return nil
+}
+
+func (c *client) keepOpen(ctx context.Context, path string) (io.ReadCloser, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", c.domain+path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating request: %w", err)
+	}
+
+	resp, err := checkStatus(c.hc.Do(req))
+	if err != nil {
+		return nil, fmt.Errorf("sending %s request: %w", path, err)
+	}
+	return resp.Body, nil
 }
 
 // login uses the username and password to login the client. Sets the returned
@@ -143,8 +248,14 @@ func (c *client) login() error {
 	return nil
 }
 
-func (c *client) get(path string) error {
-	resp, err := checkStatus(c.hc.Get(c.domain + path))
+func (c *client) get(ctx context.Context, path string) error {
+	req, err := http.NewRequestWithContext(ctx, "GET", c.domain+path, nil)
+	if err != nil {
+		return fmt.Errorf("creating request: %w", err)
+
+	}
+
+	resp, err := checkStatus(c.hc.Do(req))
 	if err != nil {
 		return fmt.Errorf("sending %s request: %w", path, err)
 	}
@@ -170,6 +281,7 @@ func checkStatus(resp *http.Response, err error) (*http.Response, error) {
 
 // Config contains all settings that can be changed with command line options.
 type Config struct {
+	testCase    int
 	clientCount int
 	domain      string
 	username    string
@@ -177,7 +289,7 @@ type Config struct {
 }
 
 func loadConfig(args []string) (*Config, error) {
-	var cfg Config
+	cfg := new(Config)
 	f := flag.NewFlagSet("args", flag.ExitOnError)
 
 	f.IntVar(&cfg.clientCount, "n", 10, "number of connections to use")
@@ -185,7 +297,9 @@ func loadConfig(args []string) (*Config, error) {
 	f.StringVar(&cfg.username, "u", "admin", "username to use for login")
 	f.StringVar(&cfg.passwort, "p", "admin", "password to use for login")
 
-	if err := f.Parse(args[1:]); err != nil {
+	test := f.String("t", "", "testcase [browser,connect]")
+
+	if err := f.Parse(args); err != nil {
 		return nil, fmt.Errorf("parsing flags: %w", err)
 	}
 
@@ -193,5 +307,19 @@ func loadConfig(args []string) (*Config, error) {
 		return nil, fmt.Errorf("invalid arguments: %s", strings.Join(flag.Args(), " "))
 	}
 
-	return &cfg, nil
+	switch *test {
+	case "browser":
+		cfg.testCase = testBrowser
+	case "connect":
+		cfg.testCase = testConnect
+	default:
+		return nil, fmt.Errorf("invalid testcase %s", *test)
+	}
+
+	return cfg, nil
 }
+
+const (
+	testBrowser = iota
+	testConnect = iota
+)
